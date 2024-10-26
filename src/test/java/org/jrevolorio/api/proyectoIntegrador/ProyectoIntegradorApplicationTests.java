4ab6fea36749b8043876107a5cf973384fc5deea
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-10-26 01:43:08 -0600

    Initialing Proyect

---
+++ src/test/java/org/jrevolorio/api/proyectoIntegrador/ProyectoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jrevolorio.api.proyectoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
